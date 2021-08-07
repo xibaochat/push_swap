6d9d658f7d5e434b4364aa00d47c01579c1c55f0
xibaochat <wmmxxbb@gmail.com>
2021-08-07 17:10:21 +0200

    Add missing stop condition

--- push_swap_program/do_push_swap.c
+++ push_swap_program/do_push_swap.c
@@ -1,5 +1,13 @@
 #include "push_swap.h"
 
+void show_chunk(int *tab, int nb_to_manage, int top)
+{
+	int j = top;
+	int k = 0;
+	while (k++ < nb_to_manage)
+		ft_putstr_w_new_line(ft_itoa(tab[j--]));
+}
+
 void	reverse_and_pb(t_stack *a, t_stack *b)
 {
 	reverse_stack_tab(a);
@@ -11,7 +19,6 @@ void	reverse_and_pb(t_stack *a, t_stack *b)
 void	rotate_and_pb(int mid, int *n, t_stack *a, t_stack *b)
 {
 	rotate_stack_tab(a);
-	ft_putstr_fd("je suis ici\n", 1);
 	if (a->tab[a->top] < mid)
 	{
 		ft_putstr_w_new_line("ra");
@@ -33,12 +40,6 @@ void	send_ele_from_a_to_b(int nb, int nb_to_b, int mid, t_stack *a, t_stack *b)
 			b->tab[b->top] = a->tab[a->top];
 			--(a->top);
 			ft_putstr_w_new_line("pb");
-			--nb_to_b;
-		}
-		//check nb from bottom of chunk
-		while (a->tab[a->top + 1 - nb_to_b] < mid && nb_to_b)
-		{
-			reverse_and_pb(a, b);
 			--nb_to_b;
 		}
 		while (a->tab[a->top] >= mid && nb_to_b)
@@ -69,6 +70,8 @@ void	do_it(int nb_to_manage, t_stack *src, t_stack *dest)
 	b = src->b_add;
 	nb_to_move = 0;
 	//move from b to a
+	if (src == a && nb_to_manage == 1)
+		return ;
 	if (arr_is_sorted(a->lens - 1, a) && b->top == -1)//a is sorted and b is vide
 		return ;
 	if (src == a && (nb_to_manage < 3 || arr_is_sorted(nb_to_manage, a)))
@@ -90,5 +93,4 @@ void	do_it(int nb_to_manage, t_stack *src, t_stack *dest)
 		}
 		do_it(nb_to_move, dest, src);
 	}
-
 }
